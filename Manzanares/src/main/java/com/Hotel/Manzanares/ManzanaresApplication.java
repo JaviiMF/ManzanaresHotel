6ea9e54a3ff5ecbcdc1358b01366ec474818b384
JaviiMF <71820330+JaviiMF@users.noreply.github.com>
2024-01-03 16:39:11 +0100

    Add files via upload

---
+++ Manzanares/src/main/java/com/Hotel/Manzanares/ManzanaresApplication.java
@@ -0,0 +1,13 @@
+package com.Hotel.Manzanares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManzanaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManzanaresApplication.class, args);
+	}
+
+}
