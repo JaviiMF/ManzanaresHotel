1a6f7215d8ef42a4397c1366421a2eddd11131a9
Ignacio Alavés <nachoalasanchez@gmail.com>
2024-01-22 14:35:02 +0100

    Creado enlace entre todas las páginas

--- Manzanares/src/main/java/com/Hotel/Manzanares/controller/ClienteController.java
+++ Manzanares/src/main/java/com/Hotel/Manzanares/controller/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ClienteController {
 	
 	@GetMapping("/login")
-	public String crear() {
+	public String loggear() {
 		return "/views/clientes/login";
 	}
 }
